3c0478efe022a30e5293d7aaf380811ef3c6c80e
Chi-Mei Wang <kimmy0929@gmail.com>
2019-09-19 17:23:50 +0900

    changes of folder location

--- Quackers/app/models/blog.rb
+++
@@ -1,4 +0,0 @@
-class Blog < ApplicationRecord
-  validates_presence_of :content
-  validates :content, length: { maximum: 140 }
-end
--- Quackers/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :blogs
-
-  root 'blogs#index'
-
-  get '/index',to: 'blogs#index'
-  get '/new',to: 'blogs#new'
-  get '/edit',to: 'blogs#edit'
-end
